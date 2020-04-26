af5ad3fe1f967fbab4b47964a6b34dba34999bee
亮亮 <1095036273@qq.con>
2020-04-26 22:03:10 +0800

    update .gitignore

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 git push -f git@github.com:Yill625/Yill625.github.io.git master
-echo "发布完成！"
+
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
