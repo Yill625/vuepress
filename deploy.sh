d990a3756da20be4b3bc4fc7ebfbc0a6724e0366
亮亮 <1095036273@qq.con>
2020-05-14 21:30:17 +0800

    取消同步修改

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,9 @@ cd docs/.vuepress/dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-# git init
-# git add -A
-# git commit -m 'deploy'
+git init
+git add -A
+git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 git push -f git@github.com:Yill625/Yill625.github.io.git master
