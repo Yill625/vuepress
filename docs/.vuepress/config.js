f3724918555c46e499b24e25fa6fd212b3d1b5f8
Ivan <1095036273@qq.com>
2020-11-07 21:52:11 +0800

    修改结构

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -160,16 +160,6 @@ module.exports = {
           ]
         },
       ],
-      '/pages/post/': [
-        {
-          title: '文章整理', // 一级菜单名称
-          collapsable: true, // false为默认展开菜单, 默认值true是折叠,
-          sidebarDepth: 2, //  设置侧边导航自动提取markdown文件标题的层级，默认1为h2层级
-          children: [
-            ['index.md', '文章整理'], //菜单名称为'子菜单1'，
-          ]
-        },
-      ]
       //...可添加多个不同的侧边栏，不同页面会根据路径显示不同的侧边栏
     }
   }
